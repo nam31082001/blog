b90721a24a7e27ddd95efcb4783d121665fa4a76
nam31082001 <caovannam785@gmail.com>
2023-04-01 00:53:32 +0700

    sorry

--- server/src/server.js
+++ server/src/server.js
@@ -1,8 +1,18 @@
 const express = require('express')
 const app = express()
+const port = 5000;
+const cors = require('cors');
+const  bodyParser = require("body-parser")
 
+app.use(cors());
+app.use(bodyParser.urlencoded(
+  {extended:false}
+))
+app.use(express.json())
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
-app.listen(3000)
+app.listen(port,()=>{
+    console.log(`Example app listening at http://localhost:${port}`)
+  })
